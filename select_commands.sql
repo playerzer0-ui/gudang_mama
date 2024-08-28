fa68a9077ee6e2339db1991c98e2080186a8e347
playerzer0-ui <playerzero745@gmail.com>
2024-08-28 13:35:58 +0700

    cleaning up sql files

--- select_commands.sql
+++
@@ -1,9 +0,0 @@
--- invoice
-
-select o.nomor_surat_jalan, o.tanggal, v.nama_vendor, g.nama_gudang, g.alamat_gudang1, g.alamat_gudang2, g.alamat_gudang3, b.nama_barang, ob.jumlah, ob.harga, ob.unit, o.status_mode
-from orders o, vendor v, barang b, gudang g, order_barang ob
-where o.gudangID = g.gudangID
-and b.barangID = ob.barangID
-and o.orderID = ob.orderID
-and o.vendorID = v.vendorID
-and status_mode = 1
--- table2_commands.sql
+++ table2_commands.sql
@@ -120,65 +120,17 @@ create table saldos(
 -- insert data
 
 insert into storages values ("NON", "none", "none", "001.111.111.111-111");
-insert into storages values ("CBA", "Catur Berkat Amartya", "jalan catur berkat", "002.111.111.111-111");
-insert into storages values ("APA", "Agraprana Paramitha Amartya", "jalan agraprana", "003.111.111.111-111");
-insert into storages values ("RBL", "Rizky Berkah Lumintu", "jalan Rizky Berkah", "004.111.111.111-111");
-insert into storages values ("BBB", "Berkah Berbagi Berkat", "jalan Berkah Berbagi", "005.111.111.111-111");
 
 insert into vendors values ("NON", "none", "none", "000.111.111.111-111");
-insert into vendors values ("WIM", "Wismilak Inti Makmur", "jln Wismilak Inti", "100.111.111.111-111");
-insert into vendors values ("ASTRA", "As Astra", "jln astra", "100.90.111.111-111");
-insert into vendors values ("COC", "Coca cola", "jln coca cola", "100.111.111.199-111");
-insert into vendors values ("NVIDIA", "video graphics", "jln video", "100.90.111.111-111");
 
 insert into customers values ("NON", "none", "none", "000.111.111.111-111");
-insert into customers values ("DED", "dedi", "jln ratulangi", "123.111.111.111-111");
-insert into customers values ("TOM", "tomi", "jln seen", "123.112.111.111-111");
-insert into customers values ("ZEN", "zeno", "jln asdf", "123.001.111.111-111");
-
-insert into products values ("RR-120-A", "Reguler Mono 120 Acetatow");
-insert into products values ("RR-100-A", "Reguler Mono 100 Acetatow");
-insert into products values ("RF-120-A", "Reguler Falvour 120 Acetatow");
-insert into products values ("RF-100-A", "Reguler Falvour 100 Acetatow");
 
 insert into orders values ("-", "NON", "-", "-", "NON", "NON", null, "-", "0");
-insert into orders values ("001/SJJ/SOME/12/2024", "CBA", "001/LPB/CBA/12/2024", "001202130", "WIM", "NON", "2024-12-03", "1212213", "1");
-insert into orders values ("002/SJJ/DOE/12/2024", "APA", "001/LPB/APA/10/2024", "001202130", "WIM", "NON", "2024-12-03", "12312213", "1");
-insert into orders values ("001/SJK/NON/10/2024", "NON", "001/SJK/NON/10/2024", "001202130", "NON", "DED", "2024-10-03", "12312213", "2");
-insert into orders values ("002/SJK/NON/10/2024", "NON", "001/SJK/NON/10/2024", "001202130", "NON", "TOM", "2024-10-03", "12000213", "2");
-
-insert into invoices values ("001/SJJ/SOME/12/2024", "2024-12-03", "001", "99.99.12312-213");
-insert into invoices values ("002/SJJ/DOE/12/2024", "2024-12-03", "002", "99.99.12312-213");
-insert into invoices values ("001/SJK/NON/10/2024", "2024-10-03", "003", "99.99.12312-213");
-insert into invoices values ("002/SJK/NON/10/2024", "2024-12-03", "004", "99.99.12312-213");
-
-insert into payments values ("001/SJJ/SOME/12/2024", "2024-12-03", "123231");
-insert into payments values ("002/SJJ/DOE/12/2024", "2024-12-03", "3000000");
-insert into payments values ("001/SJK/NON/10/2024", "2024-10-03", "223112");
-insert into payments values ("002/SJK/NON/10/2024", "2024-12-03", "55555555555");
 
 insert into repacks values ("-", NULL, "NON");
-insert into repacks values ("001/SJR/APA/12/2024", "2024-12-03", "APA");
-insert into repacks values ("002/SJR/BBB/12/2024", "2024-12-03", "BBB");
-insert into repacks values ("003/SJR/CBA/12/2024", "2024-12-03", "CBA");
 
 insert into movings values ("-", NULL, "NON", "NON");
-insert into movings values ("001/SJP/APA/12/2024", "2024-12-03", "APA", "BBB");
-insert into movings values ("002/SJP/BBB/12/2024", "2024-12-03", "BBB", "CBA");
-insert into movings values ("003/SJP/CBA/12/2024", "2024-12-03", "CBA", "APA");
 
-insert into order_products values ("001/SJJ/SOME/12/2024","-","-", "RR-120-A", 100, "tray", 2000, "something", "in");
-insert into order_products values ("001/SJJ/SOME/12/2024","-","-", "RR-100-A", 80, "tray", 100, "something", "in");
-
-insert into order_products values ("001/SJK/NON/10/2024","-","-", "RR-120-A", 100, "tray", 2000, "something", "out");
-insert into order_products values ("001/SJK/NON/10/2024","-","-", "RR-100-A", 80, "tray", 100, "something", "out");
-insert into order_products values ("002/SJK/NON/10/2024","-","-", "RR-120-A", 800, "tray", 2000, "something tax", "out");
-insert into order_products values ("002/SJK/NON/10/2024","-","-", "RR-100-A", 60, "tray", 100, "something tax", "out");
-
-insert into order_products values ("-", "-", "001/SJR/APA/12/2024", "RR-100-A", 100, "tray", 100, "repack awal", "repack_awal");
-insert into order_products values ("-", "-", "001/SJR/APA/12/2024", "RR-100-A", 100, "tray", 200, "repack last", "repack_akhir");
-insert into order_products values ("-", "-", "001/SJR/APA/12/2024", "RF-100-A", 100, "tray", 200, "repack last", "repack_akhir");
-
-insert into order_products values ("-", "001/SJP/APA/12/2024", "-", "RR-120-A", 100, "tray", 2000, "moving", "move");
-insert into order_products values ("-", "001/SJP/APA/12/2024", "-", "RR-100-A", 100, "tray", 100, "moving", "move");
-insert into order_products values ("-", "001/SJP/APA/12/2024", "-", "RF-100-A", 100, "tray", 200, "moving", "move");
+INSERT INTO `users` (`userID`, `username`, `password`, `userType`) VALUES
+('37d72912-5ad0-11ef-b5d1-5cbaef99b658', 'admin1', '$2y$10$I6HDp20xfQ.eyexX6Xu0XOmiCwmPmVGf7WuNTF6LApGFg0kxVcbIG', 1),
+('3de53767-5ad0-11ef-b5d1-5cbaef99b658', 'user1', '$2y$10$5Ymv4R2Qn3Fw/8FKRJxHmu5XAmO2G0mfXTK2naenRcsssZuvPBLVa', 0);
